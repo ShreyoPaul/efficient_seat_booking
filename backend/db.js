41d0b357e9d13ad40f845c331082cb543ed31db5
unknown <shreyopaul403@gmail.com>
2024-12-29 19:55:59 +0530

    Test

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
-const { Sequelize } = require("sequelize");
+const { Sequelize } = require("sequelize")
 
-const sequelize = new Sequelize(process.env.SUPABASE_POSTGRESQL_DB_URL);
+const sequelize = new Sequelize(process.env.SUPABASE_POSTGRESQL_DB_URL)
 
 const connectionDB = async () => {
   try {
