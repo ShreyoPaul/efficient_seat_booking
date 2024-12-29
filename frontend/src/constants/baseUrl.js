be2b5261664873c15b46ccc9e2f60b5a87777c58
unknown <shreyopaul403@gmail.com>
2024-12-29 21:12:38 +0530

    Adding endpoint to frontend baseURL

--- frontend/src/constants/baseUrl.js
+++ frontend/src/constants/baseUrl.js
@@ -1,4 +1,4 @@
-const baseURL = 'http://localhost:8000';
+const baseURL = process.env.NEXT_PUBLIC_ENDPOINT || 'http://localhost:8000';
 const authURL = `${baseURL}/api/auth`;
 const bookingURL = `${baseURL}/api/booking`;
 
